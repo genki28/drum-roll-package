0f185df1a7a03e651bf9c49eeee13929c2ba1828
大澤元気 <taiiku39@gmail.com>
2021-10-08 02:37:51 +0900

    Readmeとかちょっと修正

--- drum-roll.d.ts
+++ drum-roll.d.ts
@@ -3,4 +3,5 @@ import { DefineComponent, Plugin } from "vue";
 declare const DrumRoll: Exclude<Plugin['install'], undefined>;
 export default DrumRoll;
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 export const DrumRollComponent: DefineComponent<{}, {}, any>;
